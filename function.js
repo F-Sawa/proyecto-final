0ad952f3f2aa198ff0518d3f59065f0335dc10e7
Salvajes Diseñadores <123702376+F-Sawa@users.noreply.github.com>
2023-01-30 17:33:36 -0300

    Frontend

---
+++ function.js
@@ -0,0 +1,20 @@
+//funcion que indica el envio correcto de datos, corresponde al index 05.
+    function enviado ()
+    {
+    alert ("Datos Enviados!");
+    }
+   
+    //funcion que posibilita la navegacion entre paginas.
+     function navegar (pagina)
+    {
+        location.href= pagina;   
+    }   
+
+    //funcion que muestra una pantalla de carga, redirige en 4seg.
+    function np3 ()
+    {
+        navegar ("index03.html");
+    }
+
+  
+    
